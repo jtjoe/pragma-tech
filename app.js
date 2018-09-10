6ce9b32d2ef615cc057b0806e78af14d5480d911
Jona <32558387+jtjoe@users.noreply.github.com>
2018-09-10 13:51:32 -0700

    Added styles such as colors and fonts

---
+++ app.js
@@ -0,0 +1,22 @@
+/*var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+new Vue({
+  el: '#slideshow',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+// Define a new component called button-counter
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})*/
