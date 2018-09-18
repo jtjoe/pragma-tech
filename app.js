af43f6dc20976c7b98e846f9ce32a981069c1599
Jona <32558387+jtjoe@users.noreply.github.com>
2018-09-17 17:25:48 -0700

    Solidified website copy and added more js functionality

--- app.js
+++ app.js
@@ -1,50 +1,82 @@
-// var slides = [
-//   {
-//     image:'business-server.jpeg',
-//     caption:'_hardware and software solutions for any need__',
-//   },
-//   {
-//     image:'computer-stats.jpg',
-//     caption:'_let us help optimize your technology__',
-//   },
-//   {
-//     image:'computer-teamwork.jpg',
-//     caption:'_collaboration is key to our success__',
-//   }
-// ]
-//
-// var slide_template =
-//   '<div>
-//     <img :src="image" alt="technology">
-//     <p>{{caption}}</p>
-//   </div>';
-//
-//   Vue.component('slide_images',{
-//     props:['image','caption'],
-//     template: slide_template
-//   });
-//
-// var temp = new Vue({
-//   el: '#slideshow',
-//   data: {
-//     message: 'hello vue!',
-//   }
-// });
+// var slides = ['https://github.com/jtjoe/pragma-tech/blob/master/images/business-server.jpeg',
+// 'https://github.com/jtjoe/pragma-tech/blob/master/images/imac.jpg',
+// 'https://github.com/jtjoe/pragma-tech/blob/master/images/computer-teamwork.jpg'
+// ];
 
-Vue.component('to-top',{
-  template:'<p a href="#intro">back to top</p>'
-})
+/*
+ % slides.length
+{
+  image:'https://github.com/jtjoe/pragma-tech/blob/master/images/business-server.jpeg',
+  caption:'We offer hardware and software guidance for any need',
+},
+{
+  image:'https://github.com/jtjoe/pragma-tech/blob/master/images/imac.jpg',
+  caption:'Let us help optimize the use of technology in your business',
+},
+{
+  image:'https://github.com/jtjoe/pragma-tech/blob/master/images/computer-teamwork.jpg',
+  caption:'Collaboration is key to our success',
+},
+{
+  image:'https://github.com/jtjoe/pragma-tech/blob/master/images/discussion.jpg',
+  caption: 'insert another description'
+}
+
+Vue.component('slideimages',{
+  props:['slide'],
+  template: '<div class='view'>
+      <transition-group class='carousel' tag="div">
+        <div v-for="slide in slides" class='slide' :key="slide.image">
+          <p>{{slide.caption}}</p>
+        </div>
+      </transition-group>
+      <div class='controls'>
+        <button class='carousel-controls__button' @click="previous">prev</button>
+        <button class='carousel-controls__button' @click="next">next</button>
+      </div>
+    </div>
+ '
+});
+
+new Vue({
+  el: 'slideshow',
+  data: {
+    images:  ['https://github.com/jtjoe/pragma-tech/blob/master/images/business-server.jpeg',
+    'https://github.com/jtjoe/pragma-tech/blob/master/images/imac.jpg',
+    'https://github.com/jtjoe/pragma-tech/blob/master/images/computer-teamwork.jpg'
+    ],
+    currentSlide: 0,
+    timer: null
+  },
+  mounted: function (){
+    this.startShow();
+  },
+
+  methods:{
+    startShow: function (){
+      this.timer = setInterval(this.next, 3000);
+    },
+    stopShow: function(){
+      clearTimeout(this.timer);
+      this.timer = null;
+    },
+    next: function(){
+      this.currentSlide += 1
+    }
+  }
+});
+
+*/
+
+
+Vue.component('to-top',{ //for back to top buttons
+  template:'<p><a href="#">back to top</a></p>'
+});
 
 new Vue({
   el:'#main',
   data:{
-  }
-});
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'hi'
+    message: 'hello'
   }
 });
 
