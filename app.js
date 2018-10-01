72ca3f626b8110409aa52778570a1dc29f0bcc03
Jona <32558387+jtjoe@users.noreply.github.com>
2018-10-01 09:15:51 -0700

    Edited JS for slideshow

--- app.js
+++ app.js
@@ -1,16 +1,67 @@
+Vue.component('to-top',{ //for back to top buttons
+  template:'<p><a href="#">back to top</a></p>'
+});
 
-var slides = ['https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/business-server.jpeg',
-'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/imac.jpg',
-'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/computer-teamwork.jpg'
-];
+new Vue({//for loading component
+  el:'#main',
+  data:{  }
+});
+
+// var slides = ['https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/business-server.jpeg',
+// 'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/imac.jpg',
+// 'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/computer-teamwork.jpg'
+// ];
 
 
 new Vue({
-  el: '#slideshow',
-  data: {
-    images: slides,
-    currentSlide: 0,
-   }
+    el: 'image-slider',
+    data: {
+        images: ['https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/business-server.jpeg', 'http://i.imgur.com/PUD9HQL.jpg',
+                'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/imac.jpg',
+                'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/computer-teamwork.jpg'],
+        currentNumber: 0,
+        timer: null
+    },
+
+    mounted: function () {
+        this.startRotation();
+    },
+
+    methods: {
+        startRotation: function() {
+            this.timer = setInterval(this.next, 3000);
+        },
+
+        stopRotation: function() {
+            clearTimeout(this.timer);
+            this.timer = null;
+        },
+
+        next: function() {
+            this.currentNumber += 1
+        },
+        prev: function() {
+            this.currentNumber -= 1
+        }
+    },
+
+    computed: {
+    	currentImage: function() {
+      	return this.images[Math.abs(this.currentNumber) % this.images.length];
+      }
+    }
+});
+
+
+// new Vue({
+//   el: 'slideshow',
+//   data: {
+//     images: ['https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/business-server.jpeg',
+//     'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/imac.jpg',
+//     'https://raw.githubusercontent.com/jtjoe/pragma-tech/master/images/computer-teamwork.jpg'
+//     ],
+//     currentSlide: 0,
+//    }
   //
   // mounted: function (){
   //   this.startShow();
@@ -37,13 +88,33 @@ new Vue({
   //     return this.images[Math.abs(this.currentNumber) % this.slides.length];
   //   }
   // }
-});
+//});
 
-Vue.component('to-top',{ //for back to top buttons
-  template:'<p><a href="#">back to top</a></p>'
-});
-
-new Vue({//for loading component
-  el:'#main',
-  data:{  }
-});
+// var slideIndex = 1;
+// showSlides(slideIndex);
+//
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+//
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+//
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active";
+// }
